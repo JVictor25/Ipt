098769dbd49077bfa5b915bc12e0ac03ba32cdd9
João Victor N. Guedes <joaovictorguedes2003@hotmail.com>
2022-08-26 09:48:34 -0300

    nba cod

--- nba2k.c
+++ nba2k.c
@@ -1,7 +1,26 @@
+ /*Converso de medida nba2k*/
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    printf("Luffy disse: ");
-    printf("Kaizoku-o ore wa naru!");
+   
+   double pesok, pesol;
+   double alturam, alturap;
+   /*Vamos ler medida em libras e pés*/
+   printf("Informe a altura em pes:\n");
+
+   scanf("%lf", &alturap);
+
+   printf("Informe o peso em libras:\n");
+
+   scanf("%lf", &pesol);
+   /*Transformar para kilos e metros */
+   pesok=(pesol * 0.454);
+
+   alturam=(alturap * 0.3038);
+   /*Imprir esse respectivos valores*/
+   printf("a altura em metros é %.2lf\n", alturam);
+
+   printf("o peso em kilos é %.2lf\n", pesok);
+
     return 0;
 }
