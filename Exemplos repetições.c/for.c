ba007ae68da005f073604eac397538180fb3ba0f
João Victor N. Guedes <joaovictorguedes2003@hotmail.com>
2022-09-02 21:26:08 -0300

    repetiçao

--- Exemplos repetições.c/for.c
+++ Exemplos repetições.c/for.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int Vidro, Plástico, Papel, Orgânico, Metal, Eletrônicos, i;
 char l;
-
-int main(void) {
-  for(i=0;i==10;i++){
+int main(void) 
+{
+  for(i=0; i<g10; i++){
     scanf("%c", &l);
   switch(l){
     case 'V': Vidro++;break;
-    case 'L': Vidro++;break;
-    case 'P': Vidro++;break;
-    case 'O': Vidro++;break;
-    case 'M': Vidro++;break;
-    case 'E': Vidro++;break;}
+    case 'L': Plástico++;break;
+    case 'P': Papel++;break;
+    case 'O': Orgânico++;break;
+    case 'M': Metal++;break;
+    case 'E': Eletrônicos++;break;}
     }
   printf("Vidros: %d Plásticos: %d Papeis: %d Orgânicos: %d Metais: %d Eletrônicos: %d", Vidro, Plástico, Papel, Orgânico, Metal, Eletrônicos);
   return 0;
