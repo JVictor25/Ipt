5156848b8b24159c0bd0ab59f244d69e7dd9bd36
João Victor N. Guedes <joaovictorguedes2003@hotmail.com>
2022-09-07 20:57:49 -0300

    prova3 q3

---
+++ Prova 3/Quest2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int i, num, k, h, mediaG, erro_harmonico, erro_geometrico, erro_medio;
+int main(void) {
+ scanf("%d", &num);
+ if (num<=0)printf("Você entrou com %d, tente de novo na próxima", num);
+ else{
+for(i=0; i<=num; i++){
+    h++;
+for(i=0; i<h; i++){
+    k++;
+    if(k<10)printf(" %d ", k);
+    else printf("%d ", k);}
+    printf("\n");
+    }}
+  return 0;
+}
