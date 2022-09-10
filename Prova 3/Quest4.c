4d81938f638d42809828e4f2dafcf62f627e27f1
João Victor N. Guedes <joaovictorguedes2003@hotmail.com>
2022-09-10 09:18:51 -0300

    prova3 Quest4

--- Prova 3/Quest4.c
+++ Prova 3/Quest4.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
-int equipes, i, comp, corrida, salto, lançamento;
-char provas;
 int main(void) {
- scanf("%d", &equipes);
-for(i=0; i<=equipes; i++){
-while (provas!='F'){
-  scanf("%c", &provas);
-    if(provas=='C'){corrida++; comp++;}
-    if(provas=='S'){salto++; comp++;}
-    if(provas=='L'){lançamento++; comp++;}}}
-    printf("%d %d %d %d", corrida, salto, lançamento, comp);
+int equipes, i, corrida, salto, lancamento;
+corrida=0;
+salto=0;
+lancamento=0;
+char provas, F='F', C='C', S='S', L='L';
+
+scanf("%d", &equipes);
+for(i=0;i<equipes;i++){
+    
+  scanf("%c",&provas);
+while (provas!=F){
+    if(provas==C){corrida++;}
+    if(provas==S){salto++;}
+    if(provas==L){lancamento++;}
+    scanf("%c", &provas);}}
+    printf("%d %d %d %d", corrida, salto, lancamento, corrida+salto+lancamento);
   return 0;
 }
