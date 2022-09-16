1cf22a7876ac8ecf96fc5ca831ad65fdcf0fa50c
João Victor N. Guedes <joaovictorguedes2003@hotmail.com>
2022-09-16 09:53:09 -0300

    prova 4

--- lista condicionais.c/quest3.c
+++ lista condicionais.c/quest3.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-
-int main(void) {
-  int x1, y1, x2, y2, x, y;
-  scanf("%d %d %d %d %d %d", &x1, &y1, &x2, &y2, &x, &y);
-  if (((x<x1)||(x>x2))||((y<y1)||(y>y2)))printf("Fora!");
-  else printf("Dentro!");
-
-  return 0;
-}
+ 
